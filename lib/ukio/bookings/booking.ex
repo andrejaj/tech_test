60dc68293d441017600bad16f937b98ef68a46b4
ANDREJADELL\ajova <ajovanov@hotmail.co.uk>
2023-12-04 14:19:56 +0100

    correction to methods too many arguments.

--- lib/ukio/bookings/booking.ex
+++ lib/ukio/bookings/booking.ex
@@ -13,7 +13,7 @@ defmodule Ukio.Bookings.Booking do
     field :monthly_rent, :integer
     field :utilities, :integer
 	field :market, :string, default: "normal"
-	
+
     timestamps()
   end
 
@@ -21,12 +21,12 @@ defmodule Ukio.Bookings.Booking do
   def changeset(booking, attrs) do
     booking
     |> cast(attrs, [
-		:check_in, 
-		:check_out, 
-		:apartment_id, 
-		:monthly_rent, 
+		:check_in,
+		:check_out,
+		:apartment_id,
+		:monthly_rent,
 		:deposit,
-	    :utilities,
+	  :utilities,
 		:market
 	])
     |> validate_required([
@@ -35,18 +35,19 @@ defmodule Ukio.Bookings.Booking do
       :apartment_id,
       :monthly_rent,
       :deposit,
-	  :utilities,
-	  :market
+	    :utilities,
+	    :market
     ])
   end
-  
+
   def calculate_deposit(monthly_rent, "mars"), do: monthly_rent
   def calculate_deposit(monthly_rent, _), do: div(monthly_rent, 2)
 
   def calculate_utilities(square_meters, "mars") do
-	utilities_rate = 476.2 # utilities_rate= utilities / square_meters 
-	result = square_meters * utilities_rate
-	:erlang.floor(result)
+    utilities_rate = 476.2
+    result = square_meters * utilities_rate
+    :erlang.floor(result)
   end
-  def calculate_utilities(_square_meters, _utilities_rate, _), do: 20_000
+
+  def calculate_utilities(_square_meters, _), do: 20_000
 end
